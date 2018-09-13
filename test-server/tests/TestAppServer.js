1a055db144f79a36adb68f59ee051e6a6996e34e
ruben <rbnm.jimenez@gmail.com>
2018-09-13 01:58:31 -0300

    adding tests

--- test-server/tests/TestAppServer.js
+++ test-server/tests/TestAppServer.js
@@ -3,7 +3,7 @@
 const chai = require('chai');
 var chaiHttp = require('chai-http');
 const expect = require('chai').expect;
-
+var token;
 
 chai.use(chaiHttp);
 const url= "http://shared-server:8080";
@@ -15,6 +15,47 @@ describe('get all Servers ',() =>{
             .get('/api/servers')
             .end( function (err,res){
                 expect(res).to.have.status(401);
+                done();
+            });
+    });
+});
+
+
+describe('create server',() =>{
+    it('should fail because missing arg',(done) =>{
+        chai.request(url)
+            .post('/api/servers')
+            .send({createdBy:"autor1"})
+            .end( function (err,res){
+                expect(res).to.have.status(400);
+                done();
+            });
+    });
+});
+
+
+describe('create server',() =>{
+    it('should get token, register success',(done) =>{
+        chai.request(url)
+            .post('/api/servers')
+            .send({createdBy:"autor2", name:"server2"})
+            .end( function (err,res){
+                expect(res).to.have.status(200);
+                var object = JSON.parse(res.text);
+                token = object.server.token.token;
+                done();
+            });
+    });
+});
+
+
+describe('get all server with token',() =>{
+    it('should get all servers with success',(done) =>{
+        chai.request(url)
+            .get('/api/servers')
+            .set({'access-token':token})
+            .end( function (err,res){
+                expect(res).to.have.status(200);
                 done();
             });
     });
